67885649d5014346de600fbc41ca12f27438e54c
Hakan Hıdır <02483918@ksnet.local>
2017-10-17 11:08:59 +0300

    Add project files.

---
+++ ThirdChatBotDemoWithLuis/Dialogs/BasicLuisDialog.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace ThirdChatBotDemoWithLuis.Dialogs
+{
+    [LuisModel("Enter Your Luis App Id", "Enter Your Subscription Key")]
+    [Serializable]
+    public class BasicLuisDialog : LuisDialog<object>
+    {  
+
+        [LuisIntent("Greeting")]
+        public async Task Greeting(IDialogContext context, LuisResult result)
+        {
+            context.Call(new GreetingDialog(), Callback);
+        }
+
+        private async Task Callback(IDialogContext context, IAwaitable<object> result)
+        {
+            context.Wait(MessageReceived);
+        }
+
+    }
+}
---
+++ ThirdChatBotDemoWithLuis/Dialogs/GreetingDialog.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Luis;
+
+namespace ThirdChatBotDemoWithLuis.Dialogs
+{
+   
+    [Serializable]
+    public class GreetingDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var act = await result as Activity;
+            var message = act.Text.ToLower();
+            await context.PostAsync("Selam");
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
